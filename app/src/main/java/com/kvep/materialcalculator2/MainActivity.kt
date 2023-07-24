83095d444139e84d2578d41849dc1511d1e9bd1a
PRABHAEZHIL <prabha.ezhilarasan@gmail.com>
2023-07-24 13:49:59 +0800

    added releasenotes

--- app/src/main/java/com/kvep/materialcalculator2/MainActivity.kt
+++ app/src/main/java/com/kvep/materialcalculator2/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kvep.materialcalculator2
 
 import android.os.Bundle
+import android.provider.SyncStateContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.kvep.materialcalculator2.presentation.CalculatorScreen
@@ -9,6 +10,7 @@ import com.kvep.materialcalculator2.ui.theme.MaterialCalculatorTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+       Constants.BASE_URL
         setContent {
             MaterialCalculatorTheme {
                 // A surface container using the 'background' color from the theme
---
+++ app/src/release/java/com/kvep/materialcalculator2/Constants.kt
@@ -0,0 +1,5 @@
+package com.kvep.materialcalculator2
+
+object Constants {
+    const val BASE_URL="https://example.com"
+}
